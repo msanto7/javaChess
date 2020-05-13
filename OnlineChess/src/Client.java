f064547d42fc6566908ec1c7ad41acfa967b0461
Mike Santoro <mike.santoro42@gmail.com>
2020-05-12 20:11:07 -0400

    can connect unlimited clients to server and communicate with clientIDs

--- OnlineChess/src/Client.java
+++ OnlineChess/src/Client.java
@@ -9,8 +9,8 @@ public class Client
 { 
     public static void main(String[] args) throws IOException  
     { 
-        try
-        { 
+        //try
+        //{ 
             Scanner input = new Scanner(System.in); 
               
             // getting localhost ip 
@@ -23,35 +23,78 @@ public class Client
             DataInputStream inputStream = new DataInputStream(s.getInputStream()); 
             DataOutputStream outputStream = new DataOutputStream(s.getOutputStream()); 
       
-            // need to interact with the handler to decide what the client wants to do when they connect 
-            while (true)  
+//            // need to interact with the handler to decide what the client wants to do when they connect 
+//            while (true)  
+//            { 
+//                System.out.println(inputStream.readUTF()); 
+//                String toSend = input.nextLine(); 
+//                outputStream.writeUTF(toSend); 
+//                  
+//                // If client sends exit,close this connection  
+//                if(toSend.equals("Exit")) 
+//                { 
+//                    System.out.println("Closing this connection : " + s); 
+//                    s.close(); 
+//                    System.out.println("Connection closed"); 
+//                    break; 
+//                } 
+//                  
+//                // printing server response  
+//                String received = inputStream.readUTF(); 
+//                System.out.println(received); 
+//            } 
+//              
+//            // closing resources 
+//            input.close(); 
+//            inputStream.close(); 
+//            outputStream.close(); 
+//        }
+//        catch(Exception e){ 
+//            e.printStackTrace(); 
+//        }
+            
+            // sendMessage thread 
+            Thread sendMessage = new Thread(new Runnable()  
             { 
-                System.out.println(inputStream.readUTF()); 
-                String toSend = input.nextLine(); 
-                outputStream.writeUTF(toSend); 
-                  
-                // If client sends exit,close this connection  
-                if(toSend.equals("Exit")) 
-                { 
-                    System.out.println("Closing this connection : " + s); 
-                    s.close(); 
-                    System.out.println("Connection closed"); 
-                    break; 
+                @Override
+                public void run() { 
+                    while (true) { 
+      
+                        // read the message to deliver. 
+                        String msg = input.nextLine(); 
+                          
+                        try { 
+                            // write on the output stream 
+                            outputStream.writeUTF(msg); 
+                        } catch (IOException e) { 
+                            e.printStackTrace(); 
+                        } 
+                    } 
                 } 
-                  
-                // printing server response  
-                String received = inputStream.readUTF(); 
-                System.out.println(received); 
-            } 
+            }); 
               
-            // closing resources 
-            input.close(); 
-            inputStream.close(); 
-            outputStream.close(); 
-        }
-        catch(Exception e){ 
-            e.printStackTrace(); 
-        } 
+            // readMessage thread 
+            Thread readMessage = new Thread(new Runnable()  
+            { 
+                @Override
+                public void run() { 
+      
+                    while (true) { 
+                        try { 
+                            // read the message sent to this client 
+                            String msg = inputStream.readUTF(); 
+                            System.out.println(msg); 
+                        } catch (IOException e) { 
+      
+                            e.printStackTrace(); 
+                        } 
+                    } 
+                } 
+            }); 
+      
+            sendMessage.start(); 
+            readMessage.start();
+            
     } // Ends main 
     
 } // Ends Class 
